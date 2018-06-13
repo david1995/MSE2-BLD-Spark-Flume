7402f6373329976c0641294e3c13548b478e4ab0
David Eiwen <david-e-winlive@live.at>
2018-06-13 18:24:38 +0200

    improved python file

--- SparkFlume.Spark/SparkFlume.Spark.py
+++ SparkFlume.Spark/SparkFlume.Spark.py
@@ -1,20 +1,19 @@
 from __future__ import print_function
 import sys
-from datetime import date, datetime, timedelta
 import mysql.connector
 
+from datetime import date, datetime, timedelta
 from decimal import *
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.flume import FlumeUtils
 
-def WriteData(iter):
+def WriteData(items):
+    now = datetime.now()
     try:
         connection = mysql.connector.connect(user='docker', password='docker', host='127.0.0.1:3306', database='Products')
         cursor = cnx.cursor()
-        now = datetime.now()
-
-        for i in iter:
+        for i in items:
             cursor.execute("insert into Product values (%s, %s, %s, %s, %s)", (i['product_id'], now, i['views'], i['purchases'], i['revenue']))
             connection.commit()
 
@@ -24,23 +23,19 @@ def WriteData(iter):
         cursor.close()
         connection.close()
 
-def test(pair):
+def NewProductSum(productId): return { 'product_id': productId, 'revenue': Decimal(0.0), 'purchases': 0, 'views': 0 }
+
+def Test(pair):
     sys.stdout.flush()
-    result = {
-        'product_id': pair[0],
-        'revenue': 0.0,
-        'purchases': 0,
-        'views': 0
-    }
+    productSum = NewProductSum(pair[0])
     for it in pair[1]:
-        result['revenue'] += Decimal(it['revenue'])
-        if it['type'] == 'view': result['views'] += 1
-        else: result['purchases'] += 1
+        productSum['revenue'] += Decimal(it['revenue'])
+        if it['type'] == 'view': productSum['views'] += 1
+        else: productSum['purchases'] += 1
 
     return result
 
-def process(rdd):
-    rdd.groupBy(lambda rdd: rdd['product_id']).map(test).foreachPartition(writeToDb)
+def ProcessInput(rdd): rdd.groupBy(lambda rdd: rdd['product_id']).map(Test).foreachPartition(WriteData)
 
 
 if __name__ == "__main__":
@@ -48,17 +43,16 @@ if __name__ == "__main__":
         print("Usage: flume_wordcount.py <hostname> <port>", file = sys.stderr)
         sys.exit(-1)
 
-    sc = SparkContext(appName = "SparkFlumeStreaming")
-    sc.setLogLevel('ERROR')
+    sparkContext = SparkContext(appName = "SparkFlumeStreaming")
+    sparkContext.setLogLevel('ERROR')
 
-    ssc = StreamingContext(sc, 1)
+    streamingContext = StreamingContext(sparkContext, 1)
 
     hostname, port = sys.argv[1:]
     print('Start listening at {}:{}'.format(hostname, port))
-    kvs = FlumeUtils.createStream(ssc, hostname, int(port))
+    stream = FlumeUtils.createStream(streamingContext, hostname, int(port))
 
-    kvs.map(lambda x: x[0]).window(60, 60).foreachRDD(process)
+    stream.map(lambda x: x[0]).window(60, 60).foreachRDD(ProcessInput)
 
-    ssc.start()
-
-ssc.awaitTermination()
+    streamingContext.start()
+    streamingContext.awaitTermination()
