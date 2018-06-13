a8511384d5963cc2fb821027f4eaed040b81b9e3
David Eiwen <david-e-winlive@live.at>
2018-06-13 18:28:04 +0200

    Updated entrypoint for generator

--- GeneratorOutput/SparkFlume.EventGenerator/entrypoint.sh
+++ GeneratorOutput/SparkFlume.EventGenerator/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec dotnet SparkFlume.EventGenerator.dll generate --demo --purchasestarget http://127.0.0.1:28000 --viewstarget http://127.0.0.1:28001
+exec dotnet SparkFlume.EventGenerator.dll generate --purchasestarget http://127.0.0.1:28000 --viewstarget http://127.0.0.1:28001
